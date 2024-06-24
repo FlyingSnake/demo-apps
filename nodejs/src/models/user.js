f33d052677a4dde2bd90c5f3d2e425f13b930fbe
FlyingSnake <fasttutle131@gmail.com>
2024-06-24 19:21:59 +0900

    update

--- nodejs/src/models/user.js
+++ nodejs/src/models/user.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'db',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_DATABASE || 'test',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 const getUsers = async () => {
